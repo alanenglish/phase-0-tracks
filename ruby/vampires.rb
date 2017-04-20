bd7fc42a1b52e88f8d4613e39eb30e6c1aa97c61
Alan English <aenglish@manorhillcapital.com>
2017-04-20 17:40:26 -0500

    Add loop to process multiple employees, shorten conditional logic statements and clean up formatting

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,63 +1,67 @@
-def process_employee
+def process_employee  
 
-puts "What is your name?"
-  employee_name = gets.chomp
-    case employee_name
-    when "Drake Cula", "Tu Fang"
-      vampire_name = true
-    else
-      vampire_name = false
-     end
+  puts "How many employees will be processed today?"
+  employee_count = gets.chomp.to_i
 
-puts "How old are you?"
-  employee_age = gets.chomp.to_i
+  until employee_count == 0
+    puts "What is your name?"
+    employee_name = gets.chomp
+      case employee_name
+      when "Drake Cula", "Tu Fang"
+        vampire_name = true
+      else
+        vampire_name = false
+      end
 
-puts "What year were you born?"
-  employee_year_born = gets.chomp.to_i
-  current_year = 2017
-  confirm_age = current_year - employee_year_born
-    if confirm_age == employee_age
-      confirm_age = true
-    else 
-      confirm_age = false 
-    end
+    puts "How old are you?"
+    employee_age = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-  likes_garlic_bread = gets.chomp.downcase
-    case likes_garlic_bread
-    when "yes", "sure", "absolutely", "please", "y", "yes please", "of course"
-      likes_garlic_bread = true
-    else
-      likes_garlic_bread = false
-    end
+    puts "What year were you born?"
+    employee_year_born = gets.chomp.to_i
+    current_year = 2017
+    confirm_age = current_year - employee_year_born
+      if confirm_age == employee_age
+        confirm_age = true
+      else 
+        confirm_age = false 
+      end
 
-puts "Would you like to enroll in the company's health insurance?"
-  needs_health_insurance = gets.chomp.downcase
-    case needs_health_insurance
-    when "yes", "sure", "of course", "absolutely", "yes please", "y"
-      needs_health_insurance = true
-    else
-      needs_health_insurance = false
-    end
+    puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+    likes_garlic_bread = gets.chomp.downcase
+      case likes_garlic_bread
+      when "yes", "sure", "absolutely", "please", "y", "yes please", "of course"
+        likes_garlic_bread = true
+      else
+        likes_garlic_bread = false
+      end
 
-puts "THE RESULTS ARE IN!"
+    puts "Would you like to enroll in the company's health insurance?"
+    needs_health_insurance = gets.chomp.downcase
+      case needs_health_insurance
+      when "yes", "sure", "of course", "absolutely", "yes please", "y"
+        needs_health_insurance = true
+      else
+        needs_health_insurance = false
+      end
 
-  if vampire_name == true
-    puts "Definitely a vampire!"
-  elsif confirm_age == false && likes_garlic_bread == false && needs_health_insurance == false
-    puts "Almost certainly a vampire."
-  elsif confirm_age != true && (likes_garlic_bread != true || needs_health_insurance != true)
-    puts "Probably a vampire."
-  elsif confirm_age == true && (likes_garlic_bread == true || needs_health_insurance == false)
-    puts "Probably not a vampire."
-  else
-    puts "Results inconclusive."
+    puts "THE RESULTS ARE IN!"
+      if vampire_name
+        puts "Definitely a vampire!"
+      elsif !confirm_age && !likes_garlic_bread && !needs_health_insurance
+        puts "Almost certainly a vampire."
+      elsif !confirm_age && (!likes_garlic_bread || !needs_health_insurance)
+        puts "Probably a vampire."
+      elsif confirm_age && (likes_garlic_bread || needs_health_insurance)
+        puts "Probably not a vampire."
+      else
+        puts "Results inconclusive."
+      end
+  
+    employee_count -= 1
+      if employee_count != 0
+        puts "Lets process your next employee!"
+      end
   end
 end
 
 p process_employee
-
-
-
-
-
