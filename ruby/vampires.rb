cc0c34935295ee11d0ca8c6ec7823d8d99a7e43d
Alan English <aenglish@manorhillcapital.com>
2017-04-20 13:52:36 -0500

    Add Release 1, Interview with the Vampire

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+employee_name = gets.chomp
+
+puts "How old are you?"
+employee_age = gets.chomp
+
+puts "What year were you born?"
+employee_year_born = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+likes_garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+needs_health_insurance = gets.chomp
+
