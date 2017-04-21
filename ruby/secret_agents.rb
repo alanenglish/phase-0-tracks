e6b4e01befae47086cc719c05c13ab28d8599906
Alan English <aenglish@manorhillcapital.com>
2017-04-21 16:49:19 -0500

    Add decrypt method and pseudocode

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,7 +1,8 @@
 # - Set index variable to 0
 # - While index < the word length
-#    - Find character index value
-#    - Reassign character to the next corresponding letter
+#    - Unless index contains space
+#       - Find character index value
+#       - Reassign character to the next corresponding letter
 #    - Add 1 to index variable
 # - Print newly encrypted word
 
@@ -16,8 +17,35 @@ def encrypt(word)
   p word
 end
 
-encrypt("program")
+encrypted_word = encrypt("program")
 
+# - Set index variable to 0
+# - Set alphabet variable 
+# - While index < the word length
+#   - Unless index contains space
+#      - Use word index to find corresponding index in alphabet variable
+#      - Subtract 1 from corresponding index in alphabet
+#      - Assign that value to alpha_index variable
+#      - Use alpha_index to find corresponding letter in alphabet
+#      - Assign corresponding letter in alphabet to current word index
+#   - Add 1 to index variable
+# - Print decrypted word
+
+
+def decrypt(word)
+  index = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  while index < word.length
+    unless word[index] == " "
+    alpha_index = alphabet.index(word[index]) - 1
+    word[index] = alphabet[alpha_index]
+    end
+    index+=1
+  end
+  p word
+end
+
+decrypt(encrypted_word)
 
 
 
