56fb1d1cfababecc15b541bd8dd035d9f3c6e1b8
Alan English <aenglish@manorhillcapital.com>
2017-04-25 13:18:37 -0500

    Pseudocode for interior design hash program

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+=begin
+INTERIOR DESIGN INC. - PROSPECTIVE CLIENT
+- Ask the potential client several questions
+  - the clients name
+  - the clients age and convert to integer
+  - how many children the client has and convert to integer
+  - what their preferred decor theme is
+  - what their budget is (dollar amount)
+- Gather user data entries
+- Store data into key value pairs in hash
+- Print the hash onto the screen when all information is complete
+- Give user the ability to update a key, give user key information
+  - Allow user to skip this step by entering "none"
+  - If proper key is entered to edit, allow user to enter new value
+  - Gather input and update value of user selected key
+- Print final has program onto screen
+=end
+
