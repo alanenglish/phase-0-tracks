7440f44857a2bfb7aa1bdef69f69f7c8e5c92656
Alan English <aenglish@manorhillcapital.com>
2017-04-25 13:25:32 -0500

    Add user ability to update key value, print updated results

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -39,7 +39,38 @@ prospective_client[:color] = gets.chomp.downcase
     prospective_client[:color] = true
   end
 
-
 puts "What is your budget for this project in dollars?"
 prospective_client[:budget] = gets.chomp.gsub(/[^\d\.]/, '').to_f
+
+puts "Thank you for providing the below information."
+p prospective_client
+
+puts "Would you like to edit any of your answers above before submitting? (y/n)"
+edit = gets.chomp.downcase
+  case edit
+  when "n", "", nil
+    puts "Great! Have a wonderful day!"
+  when "y", "yes", "sure"
+    puts "Which key would you like to update (name, age, children, theme, color, budget)?"
+    edit_key = gets.chomp.to_sym
+    puts "What would you like to update #{edit_key} to?"
+    answer = ""
+      if edit_key == :age || edit_key == :children
+        answer = gets.chomp.to_i
+      elsif edit_key == :budget
+        answer = gets.chomp.gsub(/[^\d\.]/, '').to_f
+      elsif edit_key == :color
+        answer = gets.chomp
+          if answer == "color" || aswer == "colors"
+            answer = true
+          else 
+            answer = false
+          end
+      else
+        answer = gets.chomp
+      end
+  prospective_client[edit_key] = answer
+  puts "Thanks! Here is your updated information"
+  end 
+  p prospective_client
 
