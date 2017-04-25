a878e2b85c8bbe6584c98bc6858211393e1ea2ac
Alan English <aenglish@manorhillcapital.com>
2017-04-24 23:25:19 -0500

    Create hash for interior design job application

--- ruby/interior_app.rb
+++ ruby/interior_app.rb
@@ -1,1 +1,22 @@
+application = {
+  name: "Alan English",
+  address: "1009 W Webster Ave",
+  email: "alan@english.com",
+  phone: "(513) 238-3432",
+  shade: "Carolina Blue",
+  wallpaper: "Chevron",
+  ombre: "So last season"
+}
 
+p application
+
+application[:shade] = "Sky Blue"
+application[:wallpaper] = "Abstract woodsy scenes"
+application[:hired] = "Yes"
+
+
+p application.fetch(:address)
+p application.assoc(:ombre)
+
+application.delete(:hired)
+p application
