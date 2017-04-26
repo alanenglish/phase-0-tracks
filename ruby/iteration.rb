55f5b57bec41d9ea48ed4b949f5e26c3909d3e98
Alan English <aenglish@manorhillcapital.com>
2017-04-25 19:36:46 -0500

    Add .each method to array hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,5 +1,5 @@
 # Alan English and Jim Kyle
-
+# Release 0
 def dog_info
   puts "What's your dog's name?"
   dog = gets.chomp
@@ -14,14 +14,35 @@ dog_info { |dog, breed| puts "Wow, #{dog} is a lovely #{breed}!" }
 
 puts "Thanks for playing!"
 
+## Release 1
+# Ninja Turtles Hash
+turtles = {
+  Leonardo: "Blue",
+  Donatello: "Purple",
+  Raphael: "Red",
+  Michaelangelo: "Orange"
+}
+# Colors Array
+colors = ["Black", "White", "Grey", "Pink"]
 
-#   puts "What's your dog's name?"
-#   dog = gets.chomp
-#   puts "What breed is he/she?"
-#   breed = gets.chomp
+# .each Array
+puts "Original Data:"
+p colors
 
-# def dog_info(dog, breed)
-#   puts "Wow, #{dog} is a lovely #{breed}!"
-# end
+colors.each do |hue|
+  p hue
+end
 
-# dog_info( "#{dog}", "#{breed}")
+puts "After .each call:"
+p colors
+
+# .each hash
+puts "Original Data:"
+p turtles
+
+turtles.each do |name, color|
+  puts "#{name} is #{color}"
+end
+
+puts "After .each call:"
+p turtles
