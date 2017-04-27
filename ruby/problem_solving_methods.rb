6d9cc734a17d0e14311aca6d6a2cdea8bc8cd05e
Alan English <aenglish@manorhillcapital.com>
2017-04-27 18:45:05 -0500

    Release 2, sort an array, pseudocode merge sort and bubble sort, add code for bubble sort and test

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -29,16 +29,13 @@ p search_array(numbers, 25)
 
 # RELEASE 1
 
-
-# Define method that takes the 'n'th Fibonacci number
-# Assign known answer to fib(100) to variable: constant
-# Start with [0, 1] array, call it origin
-# start fibcount (to know which fib number we are on) (start at 2? 1?)
-# WHile fibcount is less than n:
-# =>  shovel sum of last two array entries back into array
-# return array
-
-# To check, use method above search_array(contant)
+# - Define method that takes the nth Fibonacci number
+# - Start with [0, 1] array = arr
+# - Start fibcount (to know which fib number we are on) at 2
+# - While fibcount is less than n:
+# =>  - Shovel sum of last two array entries back into array
+# - Return array
+# - To check, use search_array method to compare to fib(100) known value
 
 def fib(n)
   arr = [0, 1]
@@ -59,14 +56,54 @@ end
 p fib(0)
 p fib(1)
 p fib(2)
+p fib(6)
 p search_array(fib(100), 218922995834555169026) == 99
 
 
+# RELEASE 2 - Sort an Array
+
+# PSEUDOCODE FOR MERGE SORT
+# - Break integers down into units
+# - Compare each integer with adjacent integer, in pairs
+# - Sort the pairs, and then group together after sorted
+# - Compare adjacent groups by taking first integer of each
+#   and placing in new ordered list if that integer is selected
+#   and then compare the next two integers remaining in the group.
+# - Repeat step above until all elements are in new ordered list
+
+# Integrate Bubble Sort (Merge Sort too Advanced...)
+# PSUEDOCODE FOR BUBBLE SORT
+# - From left to right
+# => - Compare first two integers, and place smaller value on left side
+# => - Slide over one integer, and now compare the 2nd and 3rd integer,
+# =>   and place smaller value on left.
+# => - Slide over one integer, and now compare the 3rd and 4th integer,
+# =>   and place smaller value on left.
+# => - Continue above steps until all pairs have been sorted and the end
+# =>   of the data set has been reached.
+# - Return to the far left of the data set, and repeat pair comparisons
+#   with the first two integers that are now in place, placing smaller
+#   value to the left.
+# =>  - Repeat until all pairs have been evaluated and sorted
+# - Continue sorting pairs from left to right until all values have
+#   been sorted in order and then STOP.
 
 
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1]=array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+  array
+end
 
-
-
-
-
+bubbles = [47, 63, 88, 14, 34, 65, 21, 98, 23, 52, 82, 55, 12]
+p bubble_sort(bubbles)
 
