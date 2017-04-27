ba889cfef3b9b8e7edcecc028a2bd87aacd79ae9
Alan English <aenglish@manorhillcapital.com>
2017-04-27 11:51:05 -0500

    Create real world nested data structure with food court

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,22 @@
+food_court = {
+  chick_fil_a: {
+    restaurant: "Chick-Fil-A",
+    manager: "Roger Chickadoo",
+    menu: ["Nuggets", "Chicken Sandwich", "Waffle Fries"]
+  },
+  burger_king: {
+    restaurant: "Burger King",
+    manager: "Robert King",
+    menu: ["Whopper", "Baconator", "Curly Fries"]
+  },
+  china_wok: {
+    restaurant: "China Wok",
+    manager: "Jimmy Chang",
+    menu: ["General Changs Chicken", "Kung Pao Shrimp", "Wonton Soup"]
+  },
+  smoothie_king: {
+    restaurant: "Smoothie King",
+    manager: "JB Smoove",
+    menu: ["Tropical Paradise", "Mango Crush", "Strawberry Bonanza"]
+  }
+}
