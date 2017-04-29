e5e63e25474edc7d296e369f3b762c7206a3b159
Alan English <aenglish@manorhillcapital.com>
2017-04-28 23:39:16 -0500

    Pseudocode for encrypt secret alias method

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+# - Define encrypt method with one parameter (name)
+# - Split the given name into array 
+# - Set vowel array
+# - Set consonants array
+# - Set the index at 0
+# - For each character, first check edge case for "u" and "z"
+# - Then check each character for vowel or consonant, and change value
+# - Increase index by 1 to move to next character
+# - Change name from array to string by joining together
+# - Split back to array with just two index, first and last name
+# - Capitalize the first letter of both first and last name
+# - Swap the first and last name
+# - Join together as string
+# - Print result
