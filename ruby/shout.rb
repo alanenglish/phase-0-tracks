2491e0c48b8a07ed5129f560223bf9987ee8dc0b
Alan English <aenglish@manorhillcapital.com>
2017-05-03 09:06:27 -0500

    Add Release 1, write standalone module declaration and methods declared on self

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,16 @@
 module Shout
-  #SOON TO BE METHODS
+  
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!!!" + " :)"
+  end
+
 end
+
+#DRIVER CODE
+p Shout.yell_angrily("Damn you")
+p Shout.yelling_happily("I miss you")
+
