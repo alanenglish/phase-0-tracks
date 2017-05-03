4ce5bec65bbd0fe145315811c1471b8cac1df15d
Alan English <aenglish@manorhillcapital.com>
2017-05-02 23:41:16 -0500

    Create loop to mass produce diverse santas, set random age of each santa, and print attributes for each santa by iterating thru the array data structure

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,7 +7,7 @@ class Santa
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = rand(0..140)
   end
 
   def speak
@@ -29,32 +29,51 @@ class Santa
 end
 
 # DRIVE CODE
-nicholas = Santa.new("white walker", "dire wolf")
+p nicholas = Santa.new("white walker", "dire wolf")
 
-p nicholas.reindeer_ranking
+# p nicholas.reindeer_ranking
 
-p nicholas.get_mad_at("Cupid")
+# p nicholas.get_mad_at("Cupid")
 
-p nicholas.gender=("john snow")
+# p nicholas.gender=("john snow")
 
-nicholas.celebrate_birthday
+# nicholas.celebrate_birthday
 
-p nicholas.age
+# p nicholas.age
 
-p nicholas.ethnicity
+# p nicholas.ethnicity
 
 # nicholas.speak
 
 # nicholas.eat_milk_and_cookies("Chocolate Chip")
 
-# santas = []
+santas = []
 
-# example_genders = ["male", "female", "white walker", "agender", "bigender", "mermaid"]
-# example_ethnicities = ["black", "white", "latino", "asian", "alien", "dire wolf"]
+example_genders = ["male", "female", "white walker", "agender", "bigender", "mermaid"]
+example_ethnicities = ["black", "white", "latino", "asian", "alien", "dire wolf"]
 
 # example_genders.length.times do |i|
 #   santas << Santa.new(example_genders[i], example_ethnicities[i])
 # end
+
+# CREATE LOOP TO MASS PRODUCE DIVERSE SANTAS
+counter = 0
+until counter == 20
+  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+  counter += 1
+end
+
+# ITERATE THRU ARRAY DATA STRUCTURE AND PRINT SANTA ATTRIBUTES
+santas.each_with_index do |santa, index|
+  puts "Santa instance number: #{index+1}" 
+  puts "Age: #{santa.age}"
+  puts "Gender: #{santa.gender}"
+  puts "Ethnicity: #{santa.ethnicity}"
+  puts "Favorite Rides: #{santa.reindeer_ranking}"
+end
+
+
+
 
 
 
