ffa1859a274dc7502fda5234a552a002cc5068e2
Alan English <aenglish@manorhillcapital.com>
2017-05-12 12:44:32 -0500

    Add pseudocode for a function that reverses a given string

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// Define a function that takes a string as a parameter
+// Create a new variable inside the method, set equal to an empty string
+// Create a for loop that will start at the end of the string
+// For each letter, add them to the new variable
+// Once each letter has been looped over, return the new variable
