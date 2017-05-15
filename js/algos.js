fa18edc2544dbe9f782f340d84a357313338c35c
Alan English <aenglish@manorhillcapital.com>
2017-05-15 10:51:47 -0500

    Create function to search two objects and find if their is a key-value pair that matches, if there is return true, otherwise return false

--- js/algos.js
+++ js/algos.js
@@ -14,6 +14,31 @@ function longestPhrase(array) {
   return phrase
 };
 
+// RELEASE 1 - FIND A KEY-VALUE MATCH
+// Create function that takes parameter of two objects
+// Create new variable and set to false
+// Analyze key-value pairs to try to find a match
+// Return true if their is a match between the two objects
+// Return false if their is no match
+
+function findMatch(obj1, obj2) {
+  var matchFound = false
+  for (var i in obj1) {
+    if (obj1.hasOwnProperty(i) && obj1[i] === obj2[i]) {
+      matchFound = true;
+    }
+  }
+  return matchFound
+}
+
+// DRIVER CODE - KEEP BELOW FUNCTIONS
+console.log(findMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}))
+console.log(findMatch({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}))
+// My matchFound function only works when comparing two key-value pairs it seems like
+// I will have to come back and fix this at some point.
+var doggies = {name: "Harper", age: 1}
+var kitties = {name: "Harper", age: 4}
+console.log(findMatch(doggies, kitties))
 
 var puppies = ["Harper", "Fido", "Riley"]
 var languages = ["JavaScript", "Ruby", "Python", "HTML", "CSS"]
@@ -23,3 +48,5 @@ console.log(longestPhrase(languages))
 // In this example Jerry and Dave are equal length, so it returns Dave - curious 
 // as to how I would get this program to return both
 console.log(longestPhrase(comedians))
+
+
