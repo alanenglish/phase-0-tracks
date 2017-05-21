06010cf6deb934b9612f4e58cbb966003a1b72b9
Alan English <aenglish@manorhillcapital.com>
2017-05-21 17:41:18 -0500

    Add first half of user interface, refactor a few lines along the way

--- databases/startups/startups.rb
+++ databases/startups/startups.rb
@@ -1,5 +1,5 @@
 require 'sqlite3'
-require 'faker'
+require 'colorize'
 require_relative 'data.rb'
 
 db = SQLite3::Database.new("startups_vc.db")
@@ -54,6 +54,10 @@ def shutdown_startup(database, company)
   database.execute("DELETE FROM startups WHERE company=(?)", [company])
 end
 
+def find_startup_id(database, company)
+  database.execute("SELECT startups.id FROM startups WHERE company=(?)", [company])
+end
+
 # VENTURE CAPITALIST METHODS
 
 def create_vc(database, firm, funds_managed, established)
@@ -64,11 +68,20 @@ def update_vc(database, firm, updated_value)
   database.execute("UPDATE venture_capitalists SET funds_managed=(?) WHERE firm=(?)", [updated_value, firm])
 end
 
+def find_vc_id(database, firm)
+  database.execute("SELECT venture_capitalists.id FROM venture_capitalists WHERE firm=(?)", [firm])
+end
+
 # INVESTMENTS MADE METHODS
 
 def make_investment(database, amount_funded, year_made, startup_id, vc_id)
   database.execute("INSERT INTO investments (amount_funded, year_made, startup_id, vc_id) VALUES (?, ?, ?, ?)", [amount_funded, year_made, startup_id, vc_id])
 end
+
+def sum_of_investments(database, startup_id)
+  sum_raised = database.execute("SELECT investments.amount_funded FROM investments WHERE startup_id=(?)", [startup_id])
+end
+
 
 ### CREATE STARTUPS ###
 # create_startup(db, "Instacart", 0, "Food Tech", "San Francisco", 2012)
@@ -78,7 +91,7 @@ end
 # end
 
 
-### CREATE VENTURE CAPITALISTS ###
+### CREATE VENTURE CAPITAL FIRM ###
 # create_vc(db, "Andreessen Horowitz", 950000000, 2009)
 # create_vc(db, "Hooli Ventures", 4000000000, 2000)
 
@@ -90,7 +103,94 @@ end
 ### CREATE INVESTMENTS ###
 # make_investment(db, 20000000, 2014, 1, 1)
 
-INVESTMENTS.each do |item|
-  make_investment(db, item[0], item[1], item[2], item[3])
+# INVESTMENTS.each do |item|
+#   make_investment(db, item[0], item[1], item[2], item[3])
+# end
+
+### USER INTERFACE ###
+
+puts "WELCOME TO THE STARTUP INVESTMENT DATABASE. PLEASE ENTER YOUR NAME:"
+username = gets.chomp
+
+puts "HELLO, #{username.upcase} - GOOD TO SEE YOU AGAIN."
+
+loop do 
+  puts "WHAT WOULD YOU LIKE TO DO:"
+  puts "IF FINISHED, PLEASE TYPE 'DONE'."
+  puts "1. Register your new startup"
+  puts "2. Register your new venture capital firm"
+  puts "3. Invest in a startup"
+  puts "4. Lookup investment information by venture capital firm"
+  puts "5. Lookup investment information by startup company"
+  user_choice = gets.chomp
+    if user_choice.downcase == "done"
+      break
+    end
+    case user_choice.to_i
+    when 1
+      puts "What is your company name?"
+      company_name = gets.chomp
+      puts "What technology category is your startup in?"
+      tech_category = gets.chomp
+      puts "What city is your headquarters in?"
+      location = gets.chomp
+      puts "What year was your startup founded?"
+      year = gets.chomp.to_i 
+      create_startup(db, company_name, 0, tech_category, location, year)
+      # print the result of their entry
+    when 2
+      puts "What is the name of your VC firm?"
+      firm_name = gets.chomp
+      puts "What is your firms total AUM?"
+      total_aum = gets.chomp.to_i
+      puts "What year was your firm established?"
+      established = gets.chomp.to_i
+      create_vc(db, firm_name, total_aum, established)
+      # print the result of their entry
+    when 3
+      puts "What VC firm are you with?"
+      user_firm = gets.chomp
+      puts "What is your password?"
+      firm_password = gets.chomp
+      correct_password = false
+      USER_LOGINS.each do |firm, password|
+        if firm == user_firm && password == firm_password
+          correct_password = true
+          break
+        else
+          correct_password = false
+        end
+      end
+        if correct_password == false
+          puts "SORRY MADOFF, THE SEC HAS BEEN NOTIFIED - BETTER RUN!!"
+          puts "SYSTEM SHUTTING DOWN..."
+          exit
+        elsif correct_password == true
+          puts "ACCESS GRANTED"
+        end
+        # print startup list
+      puts "Which startup would you like to invest in?"
+      startup_name = gets.chomp
+
+      puts "How much would you like to invest?"
+      invest_amount = gets.chomp.to_i
+      sup_id = find_startup_id(db, startup_name)
+      venture_id = find_vc_id(db, user_firm)
+      make_investment(db, invest_amount, Time.now.year, sup_id, venture_id)
+      # print that specific investment
+    end
+
+
 end
+
+
+    
+
+
+
+
+
+
+
+
 
